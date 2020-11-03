367b56bb45a4f837765346c08bdb685ac506131a
sharale34 <sourabhharale8000@gmail.com>
2020-11-03 09:33:23 +0530

    Added: Welcome message is added

---
+++ main/java/com/blz/addressbookjdbc/AddressBookService.java
@@ -0,0 +1,10 @@
+package com.blz.addressbookjdbc;
+
+import java.util.logging.Logger;
+
+public class AddressBookService {
+	public static void main(String[] args) {
+		Logger log = Logger.getLogger(AddressBookService.class.getName());
+		log.info("Welcome to the Cab Invoice Generator System.");
+	}
+}
