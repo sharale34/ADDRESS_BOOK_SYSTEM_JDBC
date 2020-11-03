3fc9ce51f6844a54542b884787cd9c5f402678ce
sharale34 <sourabhharale8000@gmail.com>
2020-11-03 21:32:39 +0530

    Added: Ability to add multiple contacts to the address book database

--- test/java/com/blz/addressbookjdbc/AddressBookSystemTest.java
+++ test/java/com/blz/addressbookjdbc/AddressBookSystemTest.java
@@ -1,13 +1,21 @@
 package com.blz.addressbookjdbc;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.blz.addressbookjdbc.AddressBookService.IOService;
+
 public class AddressBookSystemTest {
+	private static Logger log = Logger.getLogger(AddressBookService.class.getName());
+
 	@Test
 	public void contactsWhenRetrievedFromDB_ShouldMatchCount() {
 		AddressBookService addressBookService = new AddressBookService();
@@ -31,7 +39,7 @@ public class AddressBookSystemTest {
 		LocalDate startDate = LocalDate.of(2018, 01, 01);
 		LocalDate endDate = LocalDate.now();
 		List<Contact> contactList = addressBookService.readContactDataForGivenDateRange(startDate, endDate);
-		Assert.assertEquals(4, contactList.size());
+		Assert.assertEquals(7, contactList.size());
 	}
 
 	@Test
@@ -48,9 +56,31 @@ public class AddressBookSystemTest {
 		AddressBookService addressBookService = new AddressBookService();
 		addressBookService.readContactData();
 		LocalDate date = LocalDate.of(2020, 11, 02);
-		addressBookService.addContactToDatabase("Shreyas", "Iyer", "Sarojini Nagar", "Delhi", "Delhi", 503125, 72024874,
+		addressBookService.addContactToDB("Shreyas", "Iyer", "Sarojini Nagar", "Delhi", "Delhi", 503125, 72024874,
 				"shreyas@gmail.com", "Personal", "Family", date);
 		boolean result = addressBookService.checkContactInSyncWithDB("Shreyas");
 		Assert.assertTrue(result);
+	}
+
+	@Test
+	public void givenContacts_WhenAddedToDB_ShouldMatchEmployeeEntries() {
+		Contact[] arrayOfEmployee = {
+				new Contact("David", "Warner", "Alwal", "Hyderabad", "Telangana", 507012, 7325331,
+						"david@gmail.com", "Casual", "Family",LocalDate.now()),
+				new Contact("Shubham", "Gill", "Durga Nagar", "Kolkata", "West Bengal", 500050, 96763129,
+						"gill@gmail.com", "Personal","Family", LocalDate.now()),
+				new Contact("A B", "de Villiers", "Miyapur", "Jaipur", "Rajasthan", 600010, 87655433,
+						"devilliers@gmail.com", "Corporate", "Family" , LocalDate.now()) };
+		AddressBookService addressBookService = new AddressBookService();
+		addressBookService.readData(IOService.DB_IO);
+		Instant start = Instant.now();
+		addressBookService.addContact(Arrays.asList(arrayOfEmployee));
+		Instant end = Instant.now();
+		log.info("Duration without thread : " + Duration.between(start, end));
+		Instant threadStart = Instant.now();
+		addressBookService.addEmployeeToPayrollWithThreads(Arrays.asList(arrayOfEmployee));
+		Instant threadEnd = Instant.now();
+		log.info("Duartion with Thread : " + Duration.between(threadStart, threadEnd));
+		Assert.assertEquals(10, addressBookService.countEntries(IOService.DB_IO));
 	}
 }
